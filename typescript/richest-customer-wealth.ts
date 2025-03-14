b59d8f9c797dc0db3e81d56a8409a46a9f9cc541
John <emailjohnthomascaballero@gmail.com>
2025-03-14 14:07:16 +0800

    Day 5

--- typescript/richest-customer-wealth.ts
+++ typescript/richest-customer-wealth.ts
@@ -28,8 +28,15 @@
 // Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
 // Output: 17
 
-function maximumWealth(accounts: number[][]): number {
-    return 0
+function maximumWealth(accounts: number[][]): any {
+    // for (let i = 0; i < accounts.length; i++) {
+    //   accounts[i].reduce((a, c) => a + c);
+    // }
+    // return accounts
+
+    return Math.max(...accounts.map(account => account.reduce((a, c) => a + c)))
 };
 
 console.log(maximumWealth([[1,2,3],[3,2,1]]));
+console.log(maximumWealth([[1,5],[7,3],[3,5]]));
+console.log(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]));

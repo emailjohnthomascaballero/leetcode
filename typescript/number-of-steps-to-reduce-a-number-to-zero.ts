c752d47349e94230aae66a7d743c68518a01d9b0
John <emailjohnthomascaballero@gmail.com>
2025-05-06 08:58:24 +0800

    Day 11

--- typescript/number-of-steps-to-reduce-a-number-to-zero.ts
+++ typescript/number-of-steps-to-reduce-a-number-to-zero.ts
@@ -7,7 +7,6 @@
  
 
 // Example 1:
-
 // Input: num = 14
 // Output: 6
 // Explanation: 
@@ -17,8 +16,8 @@
 // Step 4) 3 is odd; subtract 1 and obtain 2. 
 // Step 5) 2 is even; divide by 2 and obtain 1. 
 // Step 6) 1 is odd; subtract 1 and obtain 0.
-// Example 2:
 
+// Example 2:
 // Input: num = 8
 // Output: 4
 // Explanation: 
@@ -26,8 +25,8 @@
 // Step 2) 4 is even; divide by 2 and obtain 2. 
 // Step 3) 2 is even; divide by 2 and obtain 1. 
 // Step 4) 1 is odd; subtract 1 and obtain 0.
-// Example 3:
 
+// Example 3:
 // Input: num = 123
 // Output: 12
 
@@ -37,7 +36,13 @@
 // 0 <= num <= 106
 
 function numberOfSteps(num: number): any {
-    return num % 2 == 0 ? num / 2 : num - 1;
+    // return num % 2 == 0 ? num / 2 : num - 1;
+    let arr: number[] = [];
+    for (let index = 0; index < num; index++) {
+        let number = num % 2 == 0 ? num / 2 : num - 1;
+        arr.push(number);
+    }
+    return arr
 };
 
 console.log(numberOfSteps(14));
